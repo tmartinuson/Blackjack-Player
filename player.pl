c0fa2ea9da850555ce3461306c25ab4b2fad090d
Tristan Martinuson <tmartinuson@gmail.com>
2023-04-09 12:45:25 -0700

    added logic values for cards and actions

--- player.pl
+++ player.pl
@@ -1,5 +1,26 @@
 # TODO transfer out the basic rule logic to logic statements
 
+# Values of the cards
+value(ace, 11).
+value(king, 10).
+value(queen, 10).
+value(jack, 10).
+value(ten, 10).
+value(nine, 9).
+value(eight, 8).
+value(seven, 7).
+value(six, 6).
+value(five, 5).
+value(four, 4).
+value(three, 3).
+value(two, 2).
+
+# Actions needed
+action(hit).
+action(stand).
+action(double).
+action(split).
+
 # TODO initialize a dynamic predicate that keeps track of all 13 played cards within a deck
 
 # TODO build a cli program that takes input of a players cards and replies the basic strategy
