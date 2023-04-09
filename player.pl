a9023f77a9076dba78583cf0dca47a1391538ed7
Tristan Martinuson <tmartinuson@gmail.com>
2023-04-09 12:52:22 -0700

    calculating the hand

--- player.pl
+++ player.pl
@@ -1,6 +1,6 @@
-# TODO transfer out the basic rule logic to logic statements
+% TODO transfer out the basic rule logic to logic statements
 
-# Values of the cards
+% Values of the cards
 value(ace, 11).
 value(king, 10).
 value(queen, 10).
@@ -15,14 +15,20 @@ value(four, 4).
 value(three, 3).
 value(two, 2).
 
-# Actions needed
+% Actions needed
 action(hit).
 action(stand).
 action(double).
 action(split).
 
-# TODO initialize a dynamic predicate that keeps track of all 13 played cards within a deck
+hand([], 0).
+hand([Card|Rest], Value) :-
+    value(Card, CardValue),
+    hand(Rest, RestValue),
+    Value is CardValue + RestValue.
 
-# TODO build a cli program that takes input of a players cards and replies the basic strategy
+% TODO initialize a dynamic predicate that keeps track of all 13 played cards within a deck
 
-# TODO STRETCH incorporate dynamic predicate to count cards and add influence into the basic strategy play
+% TODO build a cli program that takes input of a players cards and replies the basic strategy
+
+% TODO STRETCH incorporate dynamic predicate to count cards and add influence into the basic strategy play
