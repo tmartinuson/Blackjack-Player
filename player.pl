2fb84b6ac6085c99764d24165fca4006cc8fec98
Tristan Martinuson <tmartinuson@gmail.com>
2023-04-09 12:33:19 -0700

    initial commit

---
+++ player.pl
@@ -0,0 +1,7 @@
+# TODO transfer out the basic rule logic to logic statements
+
+# TODO initialize a dynamic predicate that keeps track of all 13 played cards within a deck
+
+# TODO build a cli program that takes input of a players cards and replies the basic strategy
+
+# TODO STRETCH incorporate dynamic predicate to count cards and add influence into the basic strategy play
