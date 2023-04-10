1ab0f45428979640b4292846f91d8c5829c24557
Tristan Martinuson <tmartinuson@gmail.com>
2023-04-09 20:21:52 -0700

    added soft totals logic and basic_strategy template to call down

--- player.pl
+++ player.pl
@@ -21,18 +21,6 @@ action(stand).
 action(double).
 action(split).
 
-hand([], 0).
-hand([Card|Rest], Value) :-
-    value(Card, CardValue),
-    hand(Rest, RestValue),
-    Value is CardValue + RestValue.
-
-basic_strategy(C1, C2, DC, A) :-
-    hand([C1, C2], Value),
-    value(DC, DValue),
-    DValue >= 7,
-    A = hit.
-
 % -----------------  Pairs splitting strategy  -----------------
 % Facts for a pair of two's
 pairs_splitting(two, two, DC, A) :-
@@ -139,17 +127,97 @@ pairs_splitting(nine, nine, DC, A) :-
     DValue >= 10,
     A = stand.
 
-
-
 % Facts for a pair of ten's
-pairs_splitting(eight, eight, DC, A) :-
+pairs_splitting(ten, ten, DC, A) :-
     A = stand.
 
-
-
 % Facts for a pair of ace's
-pairs_splitting(eight, eight, DC, A) :-
+pairs_splitting(ace, ace, DC, A) :-
     A = split.
+
+% -----------------  Soft totals strategy  -----------------
+
+% ace, two soft totals
+soft_totals(ace, two, DC, A) :- dealer_less_than_value(DC, 5), A = hit.
+soft_totals(ace, two, DC, A) :- dealer_between_value(DC, 4, 7), A = double.
+soft_totals(ace, two, DC, A) :- dealer_greater_than_value(DC, 6), A = hit.
+soft_totals(two, ace, DC, A) :- soft_totals(ace, two, DC, A).
+
+% ace, three soft totals
+soft_totals(ace, three, DC, A) :- dealer_less_than_value(DC, 5), A = hit.
+soft_totals(ace, three, DC, A) :- dealer_between_value(DC, 4, 7), A = double.
+soft_totals(ace, three, DC, A) :- dealer_greater_than_value(DC, 6), A = hit.
+soft_totals(three, ace, DC, A) :- soft_totals(ace, three, DC, A).
+
+% ace, four soft totals
+soft_totals(ace, four, DC, A) :- dealer_less_than_value(DC, 4), A = hit.
+soft_totals(ace, four, DC, A) :- dealer_between_value(DC, 3, 7), A = double.
+soft_totals(ace, four, DC, A) :- dealer_greater_than_value(DC, 6), A = hit.
+soft_totals(four, ace, DC, A) :- soft_totals(ace, four, DC, A).
+
+% ace, five soft totals
+soft_totals(ace, five, DC, A) :- dealer_less_than_value(DC, 4), A = hit.
+soft_totals(ace, five, DC, A) :- dealer_between_value(DC, 3, 7), A = double.
+soft_totals(ace, five, DC, A) :- dealer_greater_than_value(DC, 6), A = hit.
+soft_totals(five, ace, DC, A) :- soft_totals(ace, five, DC, A).
+
+% ace, six soft totals
+soft_totals(ace, six, DC, A) :- dealer_less_than_value(DC, 3), A = hit.
+soft_totals(ace, six, DC, A) :- dealer_between_value(DC, 2, 7), A = double.
+soft_totals(ace, six, DC, A) :- dealer_greater_than_value(DC, 6), A = hit.
+soft_totals(six, ace, DC, A) :- soft_totals(ace, six, DC, A).
+
+% ace, seven soft totals
+soft_totals(ace, seven, DC, A) :- dealer_less_than_value(DC, 3), A = stand.
+soft_totals(ace, seven, DC, A) :- dealer_between_value(DC, 2, 7), A = double.
+soft_totals(ace, seven, DC, A) :- dealer_between_value(DC, 6, 9), A = stand.
+soft_totals(ace, seven, DC, A) :- dealer_between_value(DC, 8, 11), A = hit.
+soft_totals(ace, seven, DC, A) :- dealer_greater_than_value(DC, 10), A = stand.
+soft_totals(seven, ace, DC, A) :- soft_totals(ace, seven, DC, A).
+
+% ace, eight soft totals
+soft_totals(ace, eight, DC, A) :- A = stand.
+soft_totals(eight, ace, DC, A) :- soft_totals(ace, eight, DC, A).
+
+% ace, nine soft totals
+soft_totals(ace, nine, DC, A) :- A = stand.
+soft_totals(nine, ace, DC, A) :- soft_totals(ace, nine, DC, A).
+
+% ace, 10 and face cards soft totals
+soft_totals(ace, ten, DC, A) :- A = stand.
+soft_totals(ace, jack, DC, A) :- A = stand.
+soft_totals(ace, queen, DC, A) :- A = stand.
+soft_totals(ace, king, DC, A) :- A = stand.
+soft_totals(ten, ace, DC, A) :- soft_totals(ace, ten, DC, A).
+soft_totals(jack, ace, DC, A) :- soft_totals(ace, jack, DC, A).
+soft_totals(queen, ace, DC, A) :- soft_totals(ace, queen, DC, A).
+soft_totals(king, ace, DC, A) :- soft_totals(ace, king, DC, A).
+
+% -----------------  Dealer utils  -----------------
+
+dealer_greater_than_value(DC, Value) :- value(DC, DValue), DValue > Value.
+dealer_less_than_value(DC, Value) :- value(DC, DValue), DValue < Value.
+dealer_between_value(DC, Left, Right) :- dealer_greater_than_value(DC, Left), dealer_less_than_value(DC, Right).
+
+% -----------------  Basic strategy logic  -----------------
+
+basic_strategy(C1, C1, DC, A) :-
+    pairs_splitting(C1, C1, DC, A).
+basic_strategy(C1, C2, DC, A) :-
+    C1 \= C2,
+    hand_total(C1, C2, DC, A).
+
+hand([], 0).
+hand([Card|Rest], Value) :-
+    value(Card, CardValue),
+    hand(Rest, RestValue),
+    Value is CardValue + RestValue.
+
+hand_total(C1, C2, DC, A) :-
+    hand([C1, C2], Value),
+    value(DC, DValue),
+    DValue >= 7,
+    A = hit.
 
 % TODO initialize a dynamic predicate that keeps track of all 13 played cards within a deck
 
